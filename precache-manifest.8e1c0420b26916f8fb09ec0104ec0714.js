b9c69b5c804b2b881f8c6f578fb5a7b426ef7ed2
Robert Girardin <robertlgirardin@gmail.com>
2020-01-25 01:03:50 +0100

    Initial dist subtree commit

---
+++ precache-manifest.8e1c0420b26916f8fb09ec0104ec0714.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f094a40aeca208b6c2a3bbf40140e85",
+    "url": "/promenade/index.html"
+  },
+  {
+    "revision": "23e75cd067d59586bf13",
+    "url": "/promenade/static/css/main.641bcfaf.chunk.css"
+  },
+  {
+    "revision": "b5cbaa6603946e639390",
+    "url": "/promenade/static/js/2.e8843fde.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/promenade/static/js/2.e8843fde.chunk.js.LICENSE"
+  },
+  {
+    "revision": "23e75cd067d59586bf13",
+    "url": "/promenade/static/js/main.6f7f2707.chunk.js"
+  },
+  {
+    "revision": "c0004d4bac9783e71198",
+    "url": "/promenade/static/js/runtime-main.f3164ede.js"
+  },
+  {
+    "revision": "9fdce2f468e992a871d80e8ddeb9ed2e",
+    "url": "/promenade/static/media/rain2.9fdce2f4.mp4"
+  }
+]);
